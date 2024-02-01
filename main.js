134383ae56b6c2fc6c06ed81186ae61c8a6babbf
pranayadna <pranayadnaap96@gmail.com>
2024-02-01 09:56:47 +0700

    Add playRound function

--- main.js
+++ main.js
@@ -13,4 +13,39 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+    if (typeof playerSelection !== "string") {
+        return "input not string"
+    }
+
+    const player = playerSelection.toLowerCase()
+    const cpu = computerSelection.toLowerCase()
+
+    if (player === cpu) {
+        return "Same selection. The result is Tie!"
+    }
+
+    if (player === "rock" && cpu === "scissors") {
+        return "rock wins"
+    } else if (player === "paper" && cpu === "scissors") {
+        return "scissor wins"
+    } else if (cpu === "rock" && player === "scissors") {
+        return "rock wins"
+    } else if (cpu === "paper" && player === "scissors") {
+        return "scissor wins"
+    } else if (player === "paper" && cpu === "rock") {
+        return "paper wins"
+    } else if (player === "rock" && cpu === "paper") {
+        return "paper wins"
+    } else {
+        return "wrong input"
+    } 
+    
+}
+
+const playerSelection = "scissors"
+const computerSelection = getComputerChoice()
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
